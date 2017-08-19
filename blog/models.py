772e916c318be9c0091521d1c96c37bc4ea9d643
Luis Coyoy <guicho6854@gmail.com>
2017-08-18 20:26:54 -0600

    Primer envio. Se creo el modelo del blog. Se habilito el sitio de admin Se aplico un tema.

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+class Publicar(models.Model):
+    autor = models.ForeignKey('auth.User')
+    titulo = models.CharField(max_length=200)
+    texto = models.TextField()
+    fecha_creacion = models.DateTimeField(
+                default=timezone.now)
+    fecha_publica = models.DateTimeField(
+                blank=True, null=True)
+
+    def publicacion(self):
+        self.fecha_creacion = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.titulo
+# Create your models here.
