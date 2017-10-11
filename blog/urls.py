c4a2f00ca571349d5745fe5eb442386c70539d71
Luis Coyoy <guicho6854@gmail.com>
2017-10-10 19:08:02 -0600

    sitio con boton guardar y editar y muestra de detalle

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns= [
         url(r'^$', views.listar_publicacion),
-        url(r'^postear/(?P<pk>[0-9]+)/$', views.detalle_p, name='postear'),
+        url(r'^postear/(?P<pk>[0-9]+)/$', views.post_detail, name='postear'),
+        url(r'^post/new/$', views.post_new, name='post_new'),
+        url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail,  name='post_detail'),
+        url(r'^postear/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Publicar
-
+from .forms import PostearForm
+from django.shortcuts import redirect
 
 
 def listar_publicacion(request):
@@ -11,3 +12,33 @@ def listar_publicacion(request):
 def detalle_p(request,pk):
         p=get_object_or_404(Publicar,pk=pk)
         return render(request,'blog/post_detail.html', {'p':p})
+
+def post_new(request):
+    if request.method=="POST":
+       form = (PostearForm(request.POST))
+       if form.is_valid():
+           post= form.save(commit=False)
+           post.autor=request.user
+           post.fecha_publica=timezone.now()
+           post.save()
+           return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostearForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_detail(request,pk):
+    p=get_object_or_404(Publicar,pk=pk)
+    return render(request,'blog/post_detail.html', {'p':p})
+
+def post_edit(request, pk):
+        post = get_object_or_404(Publicar, pk=pk)
+        if request.method == "POST":
+            form = PostearForm(request.POST, instance=post)
+            if form.is_valid():
+                post = form.save(commit=False)
+                post.author = request.user
+                post.save()
+                return redirect('postear', pk=post.pk)
+        else:
+            form = PostearForm(instance=post)
+        return render(request, 'blog/post_edit.html', {'form': form})
